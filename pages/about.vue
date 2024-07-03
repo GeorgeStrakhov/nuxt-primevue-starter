c56f020a5b12d7bde54817acfa17737b490c2d84
George Strakhov <george.strakhov@gmail.com>
2024-07-03 13:38:29 +0200

    basic server data fetching

--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,18 @@
 <template>
     <h2>about!</h2>
-    <Button label="Check" icon="pi pi-check" @click="onClick" />
+    <Button label="Get About Info from the server" icon="pi pi-check" @click="onClick" />
+    <p v-if="message">{{ message }}</p>
 </template>
 
 <script setup>
-    const onClick = () => {
-        console.log('clicked');
+    import { ref } from 'vue'
+
+    const message = ref('')
+
+    const onClick = async () => {
+        const data = await $fetch('/api/hello', {
+            method: 'GET'
+        })
+        message.value = data.message
     }
 </script>
---
+++ server/api/hello.js
@@ -0,0 +1,5 @@
+export default defineEventHandler((event) => {
+  return {
+    message: 'yay, server message hello world'
+  }
+})
